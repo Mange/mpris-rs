6810557e9014342b76cd5c198d8ca68961728655
Magnus Bergmark <me@mange.dev>
2023-06-15 11:56:52 +0200

    Fix compile warnings for zbus

--- examples/list_players.rs
+++ examples/list_players.rs
@@ -4,7 +4,7 @@ use std::error::Error;
 #[async_std::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let mpris = Mpris::new().await?;
-    for player in mpris.players().await {
+    for player in mpris.players().await? {
         println!("{:?}", player);
     }
     Ok(())
--- src/proxies.rs
+++ src/proxies.rs
@@ -1,12 +1,18 @@
 use zbus::dbus_proxy;
 
-#[dbus_proxy]
+#[dbus_proxy(
+    default_service = "org.freedesktop.DBus",
+    interface = "org.freedesktop.DBus",
+    default_path = "/org/freedesktop/DBus"
+)]
 pub(crate) trait DBus {
     fn list_names(&self) -> zbus::Result<Vec<String>>;
 }
 
-#[dbus_proxy(interface = "org.freedesktop.DBus.Properties")]
+#[dbus_proxy(
+    interface = "org.freedesktop.DBus.Properties",
+    default_path = "/org/mpris/MediaPlayer2"
+)]
 pub(crate) trait MprisPlayer {
-    #[property]
-    fn identity(&self) -> zbus::Result<String>;
+    async fn identity(&self) -> zbus::Result<String>;
 }
